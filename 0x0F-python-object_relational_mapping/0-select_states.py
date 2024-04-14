fd888aaa43627883c2470ef5cf72c7d8b4433264
AyaAltayeb-FullStack <50461484+AyaAltayeb-FullStack@users.noreply.github.com>
2024-04-15 01:07:08 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-'''a script that lists all states from the database hbtn_0e_0_usa'''
-import MYSQLdb 
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
 from sys import argv
-
-
-
-if __name__ == '__main__'
-    db  = MYSQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-
-    cur = db.cursor
-
-    cur.execute("SELECRT * FROM states")
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
-
     for i in rows:
         print(i)
-
+    # Clean up process
     cur.close()
     db.close()
-
